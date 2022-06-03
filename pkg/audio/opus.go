43cfbe72204e8affaefe8ba57e37cb3fc13c03f7
dtinth on MBP M1 <dtinth@spacet.me>
2022-06-03 21:47:32 +0700

    Add opus

--- pkg/audio/opus.go
+++ pkg/audio/opus.go
@@ -1,6 +1,31 @@
 package audio
 
 // #cgo CFLAGS: -I${SRCDIR}/../../libs
-// #cgo LDFLAGS: -L${SRCDIR}/../../libs -lopus
+// #cgo LDFLAGS: -L${SRCDIR}/../../libs/opus/.libs -lopus
 // #include "opus/include/opus_custom.h"
 import "C"
+import "fmt"
+
+type OpusDecoder struct {
+	mode    *C.OpusCustomMode
+	decoder *C.OpusCustomDecoder
+}
+
+func CreateDecoder() (*OpusDecoder, error) {
+	var err C.int
+	mode := C.opus_custom_mode_create(48000, 128, &err)
+	if mode == nil {
+		return nil, fmt.Errorf("opus_custom_mode_create: %d", err)
+	}
+	decoder := C.opus_custom_decoder_create(mode, 2, &err)
+	if decoder == nil {
+		return nil, fmt.Errorf("opus_custom_decoder_create: %d", err)
+	}
+	return &OpusDecoder{mode, decoder}, nil
+}
+
+// Decodes an Opus packet into PCM samples.
+func (d *OpusDecoder) Decode(packet []byte, output []int16) int {
+	result := C.opus_custom_decode(d.decoder, (*C.uchar)(&packet[0]), C.int(len(packet)), (*C.opus_int16)(&output[0]), C.int(len(output)/2))
+	return int(result)
+}
---
+++ pkg/audio/opus_test.go
@@ -0,0 +1,23 @@
+package audio
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDecode(t *testing.T) {
+	decoder, err := CreateDecoder()
+	if err != nil {
+		t.Fatal(err)
+	}
+	var packet [165]byte
+	packet[0] = 0x04
+	packet[1] = 0xff
+	packet[2] = 0xfe
+	var output [960]int16
+	output[0] = 0x01
+	frames := decoder.Decode(packet[:], output[:])
+	assert.Equal(t, 128, frames)
+	assert.Equal(t, int16(0), output[0])
+}
