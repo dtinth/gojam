bd4fd7dd6d34486296f7675b1e011b1c1b345fb9
dtinth on MBP M1 <dtinth@spacet.me>
2022-09-11 20:54:08 +0700

    Extract skill level and instrument to its own

---
+++ pkg/jamulusprotocol/skilllevelid.go
@@ -0,0 +1,10 @@
+package jamulusprotocol
+
+type SkillLevelId uint8
+
+const (
+	SkillNone         SkillLevelId = 0
+	SkillBeginner     SkillLevelId = 1
+	SkillIntermediate SkillLevelId = 2
+	SkillExpert       SkillLevelId = 3
+)
